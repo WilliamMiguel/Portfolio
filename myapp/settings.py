06dccd92468053c8333d73150e38c12f8751be39
WilliamMiguel <113279478+WilliamMiguel@users.noreply.github.com>
2022-12-10 13:15:49 -0500

    Static

--- myapp/settings.py
+++ myapp/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'portfolio/static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
